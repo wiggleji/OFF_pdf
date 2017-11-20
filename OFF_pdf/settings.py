fa7260f3ccaa7d814805b16c27b4847f37e7aa0a
roamgom <roamgom@gmail.com>
2017-11-20 22:42:48 +0900

    static fix

--- OFF_pdf/settings.py
+++ OFF_pdf/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
 
 # Simplified static file serving.st
