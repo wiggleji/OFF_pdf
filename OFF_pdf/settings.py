bd5b14de2ef57e41f91bd7a9b17baa699a6ed719
roamgom <roamgom@gmail.com>
2017-11-20 23:06:13 +0900

    logging added

--- OFF_pdf/settings.py
+++ OFF_pdf/settings.py
@@ -119,6 +119,24 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1, 'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
